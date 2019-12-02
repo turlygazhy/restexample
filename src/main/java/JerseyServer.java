6e69349a904d8dd6ab0e797f8ed1078899870c4c
turlygazhy <-->
2019-12-02 21:59:50 +0600

    added some dependencies

--- src/main/java/JerseyServer.java
+++ src/main/java/JerseyServer.java
@@ -1,5 +1,4 @@
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -7,10 +6,8 @@ import org.glassfish.jersey.moxy.xml.MoxyXmlFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
-import javax.servlet.ServletException;
-
 public class JerseyServer {
-    public static void main(String[] args) throws ServletException {
+    public static void main(String[] args) {
         ResourceConfig config = new ResourceConfig();
         config.register(MoxyXmlFeature.class);
         config.register(JacksonFeature.class);
@@ -20,11 +17,6 @@ public class JerseyServer {
         Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(server, "/*");
         context.addServlet(servlet, "/services/*");
-
-        String resourceBasePath = JerseyServer.class.getResource("/WEB-INF").toExternalForm();
-        context.setWelcomeFiles(new String[]{"index.html"});
-        context.setResourceBase(resourceBasePath);
-        context.addServlet(new ServletHolder(new DefaultServlet()), "/*");
 
         try {
             server.start();
